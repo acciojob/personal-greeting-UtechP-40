712df50bfa95bd5af83e4d1de5430afa7068161d
UtechP-40 <pradeep2420pradeep@gmail.com>
2024-11-28 00:08:44 +0530

    Attempt1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [greetings,handleGreetings] = useState("")
+  const handleChange =(e)=>{
+    let value = e.target.value 
+    handleGreetings(value)
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <input type="text" onChange={handleChange} />
+        <p>Hello {greetings}!</p>
     </div>
   )
 }
